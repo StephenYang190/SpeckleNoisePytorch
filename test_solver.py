ec467d17c72d2397bf32ed2260137437d8417310
tongda <stephenyang@nuaa.edu.cn>
2021-11-09 19:19:36 +0800

    add evaluate code, some bugs exists

--- test_solver.py
+++ test_solver.py
@@ -28,40 +28,48 @@ class Solver(object):
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
             epoch_name = os.path.join(self.config.model, file)
-            print('Loading pre-trained model from %s...' % epoch_name)
 
-            # original saved file with DataParallel
-            state_dict = torch.load(epoch_name, map_location='cuda:0')
-            # create new OrderedDict that does not contain `module.`
-            frsKey, _ = list(state_dict.items())[0]
-            if 'module' in frsKey:
-                from collections import OrderedDict
-                new_state_dict = OrderedDict()
-                for k, v in state_dict.items():
-                    name = k[7:]  # remove `module.`
-                    new_state_dict[name] = v
-                # load params
-                self.net.load_state_dict(new_state_dict)
-            else:
-                self.net.load_state_dict(state_dict)
-
+            self.load_dict(epoch_name)
             self.net.eval()
-            time_s = time.time()
-            img_num = len(self.test_loader)
-            for i, data_batch in enumerate(self.test_loader):
-                images, name = data_batch['image'], data_batch['name'][0]
-                print("Proccess :" + name)
+            self.test_no_metric(outdir)
 
-                with torch.no_grad():
-                    images = images.to('cuda:0')
 
-                    preds, _ = self.net(images)
-                    pred = np.squeeze(preds).cpu().data.numpy()
-                    pred = pred * 255.0
+    def test_no_metric(self, outdir):
+        time_s = time.time()
+        img_num = len(self.test_loader)
+        for i, data_batch in enumerate(self.test_loader):
+            images, name = data_batch['image'], data_batch['name'][0]
+            print("Proccess :" + name)
 
-                    filename = os.path.join(outdir, name[:-4] + '_' + self.config.op + '_denoise.png')
-                    cv2.imwrite(filename, pred)
+            with torch.no_grad():
+                images = images.to('cuda:0')
 
-            time_e = time.time()
-            print('Speed: %f FPS' % (img_num / (time_e - time_s)))
-            print('Test Done!')
+                preds, _ = self.net(images)
+                pred = np.squeeze(preds).cpu().data.numpy()
+                pred = pred * 255.0
+
+                # filename = os.path.join(outdir, name[:-4] + '_' + self.config.op + '_denoise.png')
+                filename = os.path.join(outdir, name)
+                cv2.imwrite(filename, pred)
+
+        time_e = time.time()
+        print('Speed: %f FPS' % (img_num / (time_e - time_s)))
+        print('Test Done!')
+
+
+    def load_dict(self, epoch_name):
+        print('Loading pre-trained model from %s...' % epoch_name)
+        # original saved file with DataParallel
+        state_dict = torch.load(epoch_name, map_location='cuda:0')
+        # create new OrderedDict that does not contain `module.`
+        frsKey, _ = list(state_dict.items())[0]
+        if 'module' in frsKey:
+            from collections import OrderedDict
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                name = k[7:]  # remove `module.`
+                new_state_dict[name] = v
+            # load params
+            self.net.load_state_dict(new_state_dict)
+        else:
+            self.net.load_state_dict(state_dict)
